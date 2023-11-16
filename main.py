4895ef3246dd0b5415e9e7adc01c0a4cf97a0529
T03565 <victor.machado@sofist.co>
2023-11-16 14:34:35 -0300

    Text configuration and display added

--- main.py
+++ main.py
@@ -21,6 +21,18 @@ ground_position = (0, 232)
 # Create the clock object, which is called inside the While loop in order to set the framerate
 clock = pygame.time.Clock()
 
+# Set the font that is going to be used to display text
+font_type = "./Assets/font/Pixeltype.ttf"
+font_size = 50
+game_font = pygame.font.Font(font_type, font_size)
+
+# Create the Text surface with the Score
+text = "Score = "
+text_antialising = False
+text_color = "Black"
+text_surface = game_font.render(text, text_antialising, text_color)
+text_position = (10, 10)
+
 while True:
     # Close the game when the "X" button is clicked
     for event in pygame.event.get():
@@ -30,6 +42,7 @@ while True:
 
     screen.blit(sky_surface, sky_position)
     screen.blit(ground_surface, ground_position)
+    screen.blit(text_surface, text_position)
 
     # Updates the screen so it doesn't close after the code runs
     pygame.display.update()
