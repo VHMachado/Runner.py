d02c4a41a033db594fc47bf2594c97548f603834
Victor Hugo <victorhmds@hotmail.com>
2024-04-11 15:08:31 -0300

    Minor adjustments

--- main.py
+++ main.py
@@ -11,26 +11,25 @@ pygame.init()
 # Set game title
 pygame.display.set_caption("Runner.py")
 
-# Create game window
+# Creates the game window
 resolution = (800, 400)
 screen = pygame.display.set_mode(resolution)
 
 # Create the clock object, which is called inside the While loop in order to set the framerate
 clock = pygame.time.Clock()
 
+# Creates the player sprite that is displayed at the game's menu
+player_stand_surface, player_stand_rectangle = player_stand()
+
+# Creates the player
+player_suface, player_rectangle = player_walk_1()
+
 # Creates the enemies in the game
+enemies_rect_list = []
 snail_surface, snail_rectangle = snail()
 fly_surface, fly_rectangle = fly()
 
-enemies_rect_list = []
-
-# Creates the player sprite that is displayed on the menu
-player_stand_surface, player_stand_rectangle = player_stand()
-
-# Creates the player in the game
-player_suface, player_rectangle = player_walk_1()
-
-# Set initial variables
+# Sets initial variables
 timer = 0
 score = 0
 gravity = 0
@@ -39,7 +38,7 @@ access_menu = True
 game_active = False
 ground_position = get_ground_position()
 
-# Timer
+# Sets Timer
 enemy_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(enemy_timer, 1500)
 
@@ -53,34 +52,40 @@ while True:
         if event.type == pygame.KEYDOWN:
             # Checks if the pressed button is the SPACE key
             if event.key == pygame.K_SPACE:
-                # If the SPACE key has been pressed, the player will jump
                 if access_menu:
+                    # If the SPACE key is pressed in the game's menu, the game will start
                     access_menu = False
                     game_active = True
                     snail_rectangle = snail()[1]
                 elif game_active:
+                    # If the SPACE key is pressed during the game, the player will jump
                     if jump_cooldown == True:
                         gravity = -20
                         jump_cooldown = False
                         ground_collision = False
                 else:
+                    # If the SPACE key is pressed at the lose screen, the game will restart
                     timer = pygame.time.get_ticks()
                     snail_rectangle = snail()[1]
                     game_active = True
 
+        # The actions here are  executed only when both the game is active and the timer has ended
         if event.type == enemy_timer and game_active:
+            # Chooses whether a snail or a fly will be spawned
             if randint(0, 2):
                 enemies_rect_list.append(snail()[1])
             else:
                 enemies_rect_list.append(fly()[1])
 
     if access_menu:
+        # Sets the menu screen that's show at the beginning
         screen.fill((94, 129, 162))
         screen.blit(player_stand_surface, player_stand_rectangle)
         display_menu_text(screen)
         timer = pygame.time.get_ticks()
     elif game_active:
-        # Puts the scenario and the enemy on the screen
+        # The game "happens" here.
+        # Puts the scenario and the score on the screen
         display_scenario(screen)
         display_score(screen, timer)
 
@@ -103,16 +108,17 @@ while True:
             for enemy_rectangle in enemies_rect_list:
                 enemy_rectangle.left -= 5
 
-                # Makes the player lose if the snail collide with the player
+                # Checks collisions
                 if player_rectangle.colliderect(enemy_rectangle):
                     final_score = get_score(timer)
                     game_active = False
                     enemies_rect_list = []
 
-                # Teleports the enemy back to it's initial position if it passes the screen limit
+                # Deletes the enemy after it passess the screen limits
                 if enemy_rectangle.left <= -100:
                     enemies_rect_list.remove(enemy_rectangle)
 
+                # Draws enemies on the screen
                 if enemy_rectangle.bottom == 232:
                     screen.blit(snail_surface, enemy_rectangle)
                 else:
@@ -121,10 +127,11 @@ while True:
             enemies_rect_list = []
 
     else:
+        # Sets the lose screen that is show when the player loses
         display_scenario(screen)
         display_lose_screen(screen, final_score)
 
-    # Updates the screen so it doesn't close after the code runs
+    # Updates the screen so it doesn't close after the code runs the first time
     pygame.display.update()
 
     # Set the framerate cap to 60 frames per second
